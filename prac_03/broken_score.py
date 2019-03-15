cd3fa34af7219c4676799615fb52913cd79bc770
FrederikBezuidenhout <fbez1997@gmail.com>
2019-03-16 00:56:55 +1000

    Refactor score evaluator to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,12 +5,26 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+
+def main():
+
+    score = float(input("Enter score: "))
+    print(evaluate_score(score))
+
+    #would this be better?
+    #result = evaluate_score(score)
+    #print(result)
+
+
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score < 50:
+        return "Bad"
+
+
+main()
